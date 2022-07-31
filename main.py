cd7af3fb3d26095b2dbee510c0776c97da7d4ce6
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2022-07-31 21:38:55 +0530

    Production changes.

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-# from .Utilities import search 
-# from utils.search import Search
 from utils.drive_files_data import DriveData
 from utils.search import search_results
-# from utils import search, drive_files_data
-# from search_results import search_results
-# from archive_details import archive_details
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
-# print(Search.search_keyword("movies"))
 
 
 class SearchKeyword(Resource):
     def get(self, keywords):
         search_result = search_results(keywords)
-        # print(search_result)
-        # search_result = search_results(keywords=keywords)
         return search_result, 200
 
     
@@ -27,7 +21,6 @@ class GetDriveFiles(Resource):
     def post(self):
         data = request.get_json()
         archive_data = DriveData.drive_details(data["link"])
-        # z = archive_details(data["link"])
         return archive_data, 200
 
 
@@ -35,5 +28,7 @@ api.add_resource(SearchKeyword, '/search/<string:keywords>')
 api.add_resource(GetDriveFiles, '/drive', endpoint = 'drive')
 
 if "__main__" == __name__:
+    from waitress import serve
     # app.run()
-    app.run(debug=True)
+    serve(app, host="0.0.0.0")
+    # app.run(debug=True)
--- utils/drive_files_data.py
+++ utils/drive_files_data.py
@@ -40,49 +40,6 @@ class DriveData():
         except Exception as e:
             return e
 
-    # def search_results(keywords="movies"):
-    #     final_data = []
-
-    #     try:
-
-    #         url = f"https://www.google.com/search?q=site%3Adrive.google.com+{keywords}"
-    #         # # url = "https://www.google.com/search?q=site%3Adrive.google.com+movies&ei=OYaQYou-PJGE2roPirST-AY&ved=0ahUKEwjLvomhnP_3AhURglYBHQraBG8Q4dUDCA4&uact=5&oq=site%3Adrive.google.com+movies&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGAFKBAhGGABQowFYyyFg5SJoAnAAeACAAYABiAHQCJIBAzAuOZgBAKABAcABAQ&sclient=gws-wiz"
-    #         # page = get(url, headers=headers)
-    #         page_contents = ScrapData.scrap(url)
-    #         # print(page_contents)
-    #         # page_contents = BeautifulSoup(page.content, "html.parser")
-    #         data_parent = page_contents.find("div", {"id": "search"})
-    #         print(data_parent)
-    #         data_containers = data_parent.findAll("div", class_="yuRUbf")
-
-    #         if len(data_containers) == 0:
-    #             return False
-            
-    #         else:
-    #             id = 0
-    #             # folder = False
-    #             for container in data_containers:
-    #                 drive_link = (container.a["href"])
-    #                 # folder_check = drive_link.replace("https://drive.google.com/drive/", "")
-    #                 # if folder_check[0] == "f":
-    #                 #     folder = True
-
-    #                 # folder = True if folder_check[0] == "f" else False
-    #                 data_dict = {
-    #                     "id": id,
-    #                     "title": (container.h3.string),
-    #                     "link": drive_link,
-    #                     "path": (container.span.string),
-    #                     # "folder": folder
-    #                 }
-    #                 final_data.append(data_dict)
-    #                 id += 1
-    #             # print(final_data)
-    #             return final_data
-
-    #     except:
-    #         # print("not found")
-    #         return 0
 
 
     def drive_download_link(link=''):
